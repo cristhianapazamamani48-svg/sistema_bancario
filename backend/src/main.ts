813c745da230f084ae8142f890a20a6efdfdd478
Cristhian Apaza <cristhianapazamamani48@gmail.com>
2026-05-29 15:34:54 -0400

    feat: instalar swagger para interfaz visual de la API

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,12 +11,22 @@ async function bootstrap() {
   app.use(helmet());
   app.enableCors();
 
-  // Validaciones globales para los DTOs
+  // Validaciones globales
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // remueve propiedades no definidas en el DTO
-    forbidNonWhitelisted: true, // lanza error si mandan datos que no pedimos
-    transform: true, // transforma los tipos de datos automáticamente
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
   }));
+
+  // Configuración de Swagger (Interfaz visual para la API)
+  const config = new DocumentBuilder()
+    .setTitle('Plataforma Bancaria API')
+    .setDescription('API oficial para el simulador de banca')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Estará disponible en /api
 
   await app.listen(3000);
 }
